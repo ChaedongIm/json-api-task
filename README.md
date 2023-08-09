c48cc7dc9db866e028d9eea3462f3dd66d4770da
ChaedongIm <chaedong.im.dev@gmail.com>
2023-08-09 13:41:16 +0900

    Init: 프로젝트 세팅

---
+++ README.md
@@ -0,0 +1,9 @@
+# 과제
+
+## 요구사항
+
+- 환경 : spring-boot, jpa, mysql
+- 기능 : json 데이터를 50개 이상 수집하여 로컬 db에 저장
+    - 해당 데이터(모델)에 대한 REST API 를 제공하는 서버 구축
+    - 전반적인 REST API 동작을 체크할 수 있는 테스트 소스 구현
+    - jpa specification 기반의 확장 용이한 검색 등 임의의 추가 기능
---
+++ src/main/java/com/vv/task/JsonApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.vv.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonApiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonApiTaskApplication.class, args);
+    }
+
+}
