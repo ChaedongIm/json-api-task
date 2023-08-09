5ae2b9fbedcfb8c35f8b607a9c70326f1fe045c6
ChaedongIm <chaedong.im.dev@gmail.com>
2023-08-09 13:41:40 +0900

    Feat: 리스폰스, 에러 핸들러 세팅

---
+++ src/main/java/com/vv/task/common/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.vv.task.common;
+
+import com.vv.task.model.CommonResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<CommonResponse<Object>> globalErrorHandle(Exception ex) {
+        log.error("[{}] handled : {} ", ex.getClass().getSimpleName(), ex.getMessage());
+        String errorMessage = ex.getMessage();
+        return CommonResponse.badRequest(errorMessage != null ? errorMessage : "entity not found exception");
+    }
+}
---
+++ src/main/java/com/vv/task/model/CommonResponse.java
@@ -0,0 +1,26 @@
+package com.vv.task.model;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class CommonResponse<T> {
+    private String message;
+    private T data;
+
+    public CommonResponse(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> CommonResponse<T> success(T data) {
+        return new CommonResponse<>("ok", data);
+    }
+
+    public static <T> ResponseEntity<CommonResponse<T>> badRequest(String message) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new CommonResponse<>(message, null));
+    }
+}
