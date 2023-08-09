42e622d81a27a5b6210cf0434a8f13d69e4ad8a8
ChaedongIm <chaedong.im.dev@gmail.com>
2023-08-09 18:13:34 +0900

    Refactor: 폴더 구조 변경 (CommonResponse Class 위치 이동)

--- src/main/java/com/vv/task/common/CommonResponse.java
+++ src/main/java/com/vv/task/common/CommonResponse.java
@@ -1,4 +1,4 @@
-package com.vv.task.model;
+package com.vv.task.common;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
--- src/main/java/com/vv/task/common/GlobalExceptionHandler.java
+++ src/main/java/com/vv/task/common/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.vv.task.common;
 
-import com.vv.task.model.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
