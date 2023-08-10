615a06fecec0917e8c157c9158e520794c882129
ChaedongIm <chaedong.im.dev@gmail.com>
2023-08-10 13:15:52 +0900

    Feat: 이미지 리스트 Get API 구현

--- src/main/java/com/vv/task/images/ImageController.java
+++ src/main/java/com/vv/task/images/ImageController.java
@@ -2,8 +2,10 @@ package com.vv.task.images;
 
 import com.vv.task.common.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -11,8 +13,10 @@ public class ImageController {
 
     private final ImageService imageService;
 
-    @GetMapping("/api/v1/images")
-    public CommonResponse<Object> getHello() {
-        return CommonResponse.success(imageService.getImageList());
+    @GetMapping("/api/v1/list")
+    public CommonResponse<?> getList(Pageable pageable) {
+        return CommonResponse.success(imageService.findAll(pageable));
     }
+
+
 }
--- src/main/java/com/vv/task/images/ImageService.java
+++ src/main/java/com/vv/task/images/ImageService.java
@@ -1,83 +1,39 @@
 package com.vv.task.images;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vv.task.images.model.Image;
 import com.vv.task.images.model.ImageResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ImageService {
-
-    private final ObjectMapper objectMapper;
-    private final RestTemplate restTemplate;
     private final ImageRepository imageRepository;
 
-    @Value("${open-api.apikey}")
-    private String API_KEY;
-    private String MOBILE_OS = "IOS";
-    private String MOBILE_APP = "MyApp";
-    private String TYPE_JSON = "json";
-    private String BASE_URL = "http://apis.data.go.kr";
-
-    private String LIST_URL = "/B551011/PhotoGalleryService1/galleryList1";
-    private Integer NUM_OF_ROWS = 50;
-
-    @Transactional
-    public Object getImageList() {
-        URI uri = UriComponentsBuilder
-                .fromUriString(BASE_URL + LIST_URL)
-                .queryParam("serviceKey", API_KEY)
-                .queryParam("MobileApp", MOBILE_APP)
-                .queryParam("MobileOS", MOBILE_OS)
-                .queryParam("_type", TYPE_JSON)
-                .queryParam("numOfRows", NUM_OF_ROWS)
-                .encode()
-                .build()
-                .toUri();
-        ResponseEntity<JsonNode> response = restTemplate.exchange(uri, HttpMethod.GET, null, JsonNode.class);
-        HttpStatus statusCode = response.getStatusCode();
-        if (!statusCode.is2xxSuccessful()) {
-            throw new HttpServerErrorException(statusCode);
-        }
-
-        List<Image> images = getImagesFromResponse(response);
-        imageRepository.saveAll(images);
-
-        return "Save 성공";
+    @Transactional(readOnly = true)
+    public Page<ImageResponseDto> findAll(Pageable pageable) {
+        return imageRepository.findAll(pageable)
+                .map(image -> ImageResponseDto.from(image));
     }
 
-    private List<Image> getImagesFromResponse(ResponseEntity<JsonNode> response) {
-        List<Image> images = new ArrayList<>();
-        response.getBody()
-                .get("response")
-                .get("body")
-                .get("items")
-                .get("item")
-                .iterator()
-                .forEachRemaining(node -> {
-                    try {
-                        ImageResponseDto imageDto = objectMapper.treeToValue(node, ImageResponseDto.class);
-                        images.add(Image.from(imageDto));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
+    @Transactional(readOnly = true)
+    public ImageResponseDto findByImageId() {
+        return null;
+    }
 
-        return images;
+    @Transactional
+    public ImageResponseDto create() {
+        return null;
+    }
+
+    @Transactional
+    public ImageResponseDto update() {
+        return null;
+    }
+
+    @Transactional
+    public void delete() {
     }
 }
